146cd8dfbed55363fda0f8e7f16a5ea85ccac54b
JRGCast <joao.ralph1@gmail.com>
2021-08-22 18:09:08 -0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,36 @@ import './App.css';
 const App: React.FC = () => {
   const [list, setList] = useState<string[]>([]);
 
-  const handleListClick = ({currentTarget}: any) => {
-    const newStyle = {textDecoration: 'line-through'}
-    const currLi = document.getElementById(`${currentTarget.id}`)
-    const theStyle = currLi?.getAttribute('style');
-    if(theStyle === null || theStyle !== 'text-decoration:line-through') {
-      currLi?.setAttribute('style', 'text-decoration:line-through')
-    } else {
-      currLi?.setAttribute('style', 'text-decoration:none')
-    }
-      
-    console.log(currLi, theStyle)
+  const handleListClick = ({ currentTarget }: any) => {
+    const replaceIdToLi = currentTarget.id.replace('btn', 'li');
+    const currLi = document.getElementById(replaceIdToLi)!;
+    const removedElement = list.filter(elemen => elemen !== currLi.innerText)
+    setList(removedElement)
   }
+
   const mapList = list.map((item: string, index: number) => {
-  return (<li id={`${index}`} key={index} onClick={handleListClick}>{item}</li>)
+    return (
+      <div>
+        <li id={`${ index }-li`} key={`${ index }-li`}>{item}</li>
+        <button id={`${index}-btn`} key={`${ index }-btn`} type='button' onClick={handleListClick}>Excluir da lista</button>
+      </div>)
   })
-  
+
   const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if(e.key === 'Enter') handleSubmit(e)
+    if (e.key === 'Enter') handleSubmit(e)
   }
-  const handleSubmit = (e:any) => {
+  const handleSubmit = (e: any) => {
     const theInput = (document.getElementById('todo-input') as HTMLInputElement);
     e.preventDefault();
-      setList([...list, theInput.value])
-      theInput.value = ''
+    setList([...list, theInput.value])
+    theInput.value = ''
   }
   return (
     <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <label htmlFor='todo-input'> Coloque sua tarefa aqui:&nbsp;
-        <input id='todo-input' type='text' placeholder='tarefas' onKeyDown={handleEnter} ></input>
+          <input id='todo-input' type='text' placeholder='tarefas' onKeyDown={handleEnter} ></input>
         </label>
         <button type='submit' onClick={(e) => handleSubmit(e)}>Adicionar tarefa</button>
         <ol id='todo-ol'>
