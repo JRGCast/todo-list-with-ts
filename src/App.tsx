1e75e4879f98e4b13a2498a76525c4cc500f6ab0
JRGCast <joao.ralph1@gmail.com>
2021-08-22 15:32:54 -0300

    quase feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [list, setList] = useState<string[]>([]);
+
+  const handleListClick = ({currentTarget}: any) => {
+    const newStyle = {textDecoration: 'line-through'}
+    const currLi = document.getElementById(`${currentTarget.id}`)
+    const theStyle = currLi?.getAttribute('style');
+    if(theStyle === null || theStyle !== 'text-decoration:line-through') {
+      currLi?.setAttribute('style', 'text-decoration:line-through')
+    } else {
+      currLi?.setAttribute('style', 'text-decoration:none')
+    }
+      
+    console.log(currLi, theStyle)
+  }
+  const mapList = list.map((item: string, index: number) => {
+  return (<li id={`${index}`} key={index} onClick={handleListClick}>{item}</li>)
+  })
+  
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter') handleSubmit(e)
+  }
+  const handleSubmit = (e:any) => {
+    const theInput = (document.getElementById('todo-input') as HTMLInputElement);
+    e.preventDefault();
+      setList([...list, theInput.value])
+      theInput.value = ''
+  }
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label htmlFor='todo-input'> Coloque sua tarefa aqui:&nbsp;
+        <input id='todo-input' type='text' placeholder='tarefas' onKeyDown={handleEnter} ></input>
+        </label>
+        <button type='submit' onClick={(e) => handleSubmit(e)}>Adicionar tarefa</button>
+        <ol id='todo-ol'>
+          {mapList}
+        </ol>
       </header>
     </div>
   );
